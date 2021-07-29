e6544038130a5f765ab4d491ac2d31755ef751e2
RohanJoshi28 <rjoshi6@gmail.com>
2021-07-29 16:01:50 -0400

    improved responsiveness of draggable filter to different screen sizes and consistency

--- screens/Filter.js
+++ screens/Filter.js
@@ -1,13 +1,23 @@
 import React, {useState} from 'react';
-import { Text, View, StyleSheet, Image, Platform } from 'react-native';
+import { Text, View, StyleSheet, Image, Platform, Dimensions } from 'react-native';
 import Draggable from 'react-native-draggable';
 
 export default function Filter({navigation}){
 
+    //location of the draggable filter (x,y) coordinates
     const [dragX, setDragX] = useState(0);
     const [dragY, setDragY] = useState(0);
+
+    //distance of draggable filter from target
     const [xDist, setXDist] = useState(100);
     const [yDist, setYDist] = useState(100);
+
+    //height and width of image (used to perform calculations for target of draggable filter)
+    const [imageWidth, setImageWidth] = useState(300);
+    const [imageHeight, setImageHeight] = useState(200);
+
+    //dimensions of the draggable container (used for responsiveness to different screen sizes)
+    const [dragContainerDim, setDragContainerDim] = useState({width: 0, height: 0, x: 0, y: 0});
 
     return (
         
@@ -17,44 +27,63 @@ export default function Filter({navigation}){
             <Text style={styles.paragraph}>Filter on Image</Text>
 
             {/*View that contains an image and a draggable filter ron top of it*/}
-            <View style={styles.dragContainer}>
+            <View style={styles.dragContainer} onLayout={(event) => {
 
-                <Image style={{width: 300, height: 200}} source={require('../assets/grayscale_face_image.jpg')}/>
+                //get dimensions of container and
+                var {x, y, width, height} = event.nativeEvent.layout;
+                setDragContainerDim({width: width, height: height, x: x, y: y});
+                
+            }}>
+
+                {/* main image of face */}
+                <Image style={{width: imageWidth, height: imageHeight}} source={require('../assets/grayscale_face_image.jpg')}/>
 
                 {/*Draggable filter*/}
                 <Draggable 
-                imageSource={require('../assets/filter_drawing (1).png')} 
+                imageSource={require('../assets/filter_drawing.png')} 
 
+                //size of draggable filter for android
                 renderSize={35} 
 
+                //original starting point of the filter on the image (top left corner)
+                x={dragContainerDim["width"]/2-imageWidth/2}
+                y={dragContainerDim["height"]/2-imageHeight/2}
+
                 /*set the minimum and maximum bounds where the draggable item can go to the bounds of the actual image */
-                maxX={230} minX={0} minY={0} maxY={150} 
+                maxX={dragContainerDim["width"]/2+imageWidth/2} 
+                minX={dragContainerDim["width"]/2-imageWidth/2} 
+                minY={dragContainerDim["height"]/2-imageHeight/2} 
+                maxY={dragContainerDim["height"]/2+imageHeight/2}
 
                 /*set the draggable filter's state location when the user releases the filter. Calculate the distance between the filter and the nose bridge (where the filter 'similarity' is highest) */
-                onDragRelease={(e) => {setDragX(e.nativeEvent.locationX), 
-                                    setDragY(e.nativeEvent.locationY), 
-                                    setXDist(Math.abs(40-dragX), 
-                                    setYDist(Math.abs(27-dragY)))}} 
+                onDragRelease={(e) => {
+                                    setDragX(e.nativeEvent.pageX), 
+                                    setDragY(e.nativeEvent.pageY), 
 
-                /*When using ios, you can use the children parameter to have more customization over the image */
-                children={   Platform.OS==="ios" || "web"  ?    (<Image style={{width: 70, height: 50, position:"absolute"}} source={require('../assets/filter_drawing (1).png')}/>) : null}
+                                    //target of filter is near the middle of the image (nose bridge)
+                                    setXDist(Math.abs(dragContainerDim["width"]/2-dragX)), 
+                                    setYDist(Math.abs(dragContainerDim["height"]/1.1-dragY))}} 
+
+                /*When using ios, you can use the children parameter to have more customization over the filter image */
+                children={   Platform.OS==="ios" || "web"  ?    (<Image style={{width: 70, height: 50}} source={require('../assets/filter_drawing.png')}/>) : null}
 
                 />
 
             </View>
             
             {/*Invert the distance between the filter and its target. The smaller the distance the higher the 'similarity' Also, round to the nearest integer and then min with 100 because sometimes inversion can cause division by a very small number (and thus grow to infinity) */}
+
             <Text style={styles.paragraph}>
                 Current Similarity Match:
                 {
-                Math.min(Math.round(1/xDist*100 + 1/yDist * 100), 100)
+                    Math.min(Math.round(1/(xDist+yDist) * 200), 100)
                 }
             </Text>
             
-            {/*If the inverted distance is lower than a certain threshold, then display the answer (which the user got correct) */}
+            {/*If the inverted distance is lower than a certain threshold for both x and y coordinates, then display the answer (which the user got correct) */}
             <Text style={styles.paragraph}>
                 {
-                (Math.round(1/xDist*100 + 1/yDist * 100))>20 ? "The filter matches up closest to the nose bridge because it is a vertical line!" : ""
+                    (Math.round(1/xDist*100)>5 && Math.round((1/yDist * 100))>5) ? "The filter matches up closest to the nose bridge because it is a vertical line!" : ""
                 }
             </Text>
 
@@ -65,7 +94,6 @@ export default function Filter({navigation}){
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     paddingTop: 15,
     backgroundColor: '#ecf0f1',
@@ -80,5 +108,6 @@ const styles = StyleSheet.create({
   dragContainer: {
     justifyContent: "center",
     textAlign: "center",
+    alignItems: "center",
   },
 });
