a786e329cced4b1418453bf31ec47c3bd72ca599
RohanJoshi28 <rjoshi6@gmail.com>
2021-09-02 20:33:04 -0400

    upgraded linting, made sure the user cannot modify zoom level (only the animation can), and introduced gradual zoom at start.

--- components/Calculation.js
+++ components/Calculation.js
@@ -1,51 +1,73 @@
-import React, { useState, useEffect, useRef, createRef } from 'react'
-import { View, StyleSheet, Image, Platform, Dimensions, Animated, Text } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { View, StyleSheet, Image, Platform, Dimensions, Animated } from 'react-native'
 import Draggable from 'react-native-draggable'
 import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView'
-import { addListener } from 'expo-media-library';
 
 export default function Calculation ({ calculations, setCalculations, imageXOffset, imageYOffset }) {
-
-  const moveToCoords = useRef(new Animated.ValueXY({x: 0, y: 0})).current;
-
-  const [allowZoom, setAllowZoom] = useState(true)
+  const moveToCoords = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current
 
   useEffect(() => {
-    function startZoom(){
+    function startZoom () {
       setCalculations(0)
-      zoomableViewRef.current?.zoomBy(2)
-      zoomableViewRef.current?.moveTo(0,0)
-    }
-    Animated.delay(600).start(()=>{
-    startZoom()
-    Animated.timing(
-      moveToCoords,
-      {
-        toValue: {x: imageWidth*10, y: 0},
-        duration: 8000,
-        useNativeDriver: true
+      let currentZoom = 0
+      function startAnim () {
+        Animated.sequence([
+          Animated.delay(30)
+        ]).start(() => {
+          zoomableViewRef.current?.zoomBy(0.03)
+          zoomableViewRef.current?.moveTo(10, 10)
+          currentZoom += 0.03
+          if (currentZoom >= 2) {
+            return
+          }
+          startAnim()
+        })
       }
-    ).start(()=>{
-
-      zoomableViewRef.current?.zoomBy(-1.95);
-      moveToCoords.removeAllListeners()
-      setAllowZoom(false)
+      startAnim()
+    }
+    Animated.delay(600).start(() => {
+      startZoom()
+      Animated.sequence([
+        Animated.delay(4000),
+        Animated.timing(
+          moveToCoords,
+          {
+            toValue: { x: imageWidth * 8, y: 0 },
+            duration: 8000,
+            useNativeDriver: true
+          }
+        )
+      ]).start(() => {
+        let currentZoom = 2
+        function endAnim () {
+          Animated.sequence([
+            Animated.delay(5)
+          ]).start(() => {
+            zoomableViewRef.current?.zoomBy(-0.03)
+            currentZoom -= 0.03
+            if (currentZoom <= 0) {
+              moveToCoords.removeAllListeners()
+              setCalculations(0)
+              return
+            }
+            endAnim()
+          })
+        }
+        endAnim()
+      })
     })
-    });
-    
   }, [moveToCoords])
 
+  const zoomableViewRef = useRef(null)
 
-  const zoomableViewRef = useRef(null);
-
-  var tenth = 0
-    moveToCoords.addListener((value)=>{
-    tenth+=1
-    if ((tenth%100)==0){
-      setCalculations(Math.round(((1/Math.abs(1000-value.x)**1.5)*10000000000)))
+  let tenth = 0
+  moveToCoords.addListener((value) => {
+    tenth += 1
+    if ((tenth % 100) === 0) {
+      setCalculations(Math.round(((1 / Math.abs(1000 - value.x) ** 1.5) * 10000000000)))
     }
-    
-    zoomableViewRef.current?.moveTo(value.x%imageWidth, Number.parseInt(value.x/(imageWidth))*100-300)
+
+    zoomableViewRef.current?.moveTo(value.x % imageWidth, Number.parseInt(value.x / (imageWidth)) * 100 - 300)
   })
 
   // height and width of image (used to perform calculations for target of draggable filter)
@@ -68,46 +90,46 @@ export default function Calculation ({ calculations, setCalculations, imageXOffs
         }}
       >
 
-        <View style={[styles.zoomableWrapper, {width: imageWidth, height: imageHeight}]}>
-        {/* main image of face */}
-        <ReactNativeZoomableView
-          ref={zoomableViewRef}
-          zoomEnabled={allowZoom}
-          maxZoom={30}
-          minZoom={1}
-          zoomStep={0.25}
-          initialZoom={1}
-          bindToBorders
-          style={styles.zoomView}
-        >
+        <View style={[styles.zoomableWrapper, { width: imageWidth, height: imageHeight }]}>
+          {/* main image of face */}
+          <ReactNativeZoomableView
+            ref={zoomableViewRef}
+            zoomEnabled={false}
+            maxZoom={30}
+            minZoom={1}
+            zoomStep={0.25}
+            initialZoom={1}
+            bindToBorders
+            style={styles.zoomView}
+          >
             <Image
-            style={{ width: imageWidth, height: imageHeight }}
-            source={require('../assets/obama_pixelated_medium.png')}
+              style={{ width: imageWidth, height: imageHeight }}
+              source={require('../assets/obama_pixelated_medium.png')}
             />
-        </ReactNativeZoomableView>
-        {/* Draggable filter */}
-        <Draggable
-          imageSource={require('../assets/red_box.png')}
+          </ReactNativeZoomableView>
+          {/* Draggable filter */}
+          <Draggable
+            imageSource={require('../assets/red_box.png')}
           // size of draggable filter for android
-          renderSize={130}
-          disabled={true}
+            renderSize={130}
+            disabled
           // original starting point of the filter on the image (top left corner)
-          x={(dragContainerDim.width / 4.3)}
-          y={(dragContainerDim.height / 4.5)}
+            x={(dragContainerDim.width / 4.3)}
+            y={(dragContainerDim.height / 4.5)}
           /* set the minimum and maximum bounds where the draggable item can go to the bounds of the actual image */
-          maxX={dragContainerDim.width / 2 + imageWidth / 2}
-          minX={dragContainerDim.width / 2 - imageWidth / 2}
-          minY={dragContainerDim.height / 2 - imageHeight / 2}
-          maxY={dragContainerDim.height / 2 + imageHeight / 2}
-          animatedViewProps={{ opacity: 0.3 }}
-        >
-          {
+            maxX={dragContainerDim.width / 2 + imageWidth / 2}
+            minX={dragContainerDim.width / 2 - imageWidth / 2}
+            minY={dragContainerDim.height / 2 - imageHeight / 2}
+            maxY={dragContainerDim.height / 2 + imageHeight / 2}
+            animatedViewProps={{ opacity: 0.3 }}
+          >
+            {
             /* When using ios, you can use the children parameter to have more customization over the filter image */
             (Platform.OS === 'ios' || Platform.OS === 'web')
               ? (<Image style={styles.filterImage} source={require('../assets/red_box.png')} />)
               : null
           }
-        </Draggable>
+          </Draggable>
         </View>
       </View>
     </View>
@@ -139,13 +161,13 @@ const styles = StyleSheet.create({
   filterImage: {
     width: Dimensions.get('window').width / 3.9,
     height: Dimensions.get('window').width / 3.3
-  }, 
+  },
   zoomView: {
     padding: 10,
     backgroundColor: 'transparent'
-  }, 
+  },
   zoomableWrapper: {
-    overflow: 'hidden',
+    overflow: 'hidden'
   }
 
 })
