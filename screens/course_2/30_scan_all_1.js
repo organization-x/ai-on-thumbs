ff24a3bd8d6381eefcfa75b47682b5fc731d4bdb
ZacharyBrammer <zacharyj.brammer@gmail.com>
2021-10-07 17:06:25 -0400

    created screen 30. Nav buttons will need to be setup once next screens done

---
+++ screens/course_2/30_scan_all_1.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import LessonButton from "../../components/LessonButton";
+
+export default function Course2ScanAll1({ navigation }) {
+  return (
+    <LinearGradient colors={["#8976C2", "#E6E8FB"]} style={styles.container}>
+      <View style={styles.textSection}>
+        <Text style={styles.mainText}>
+          Ok, now we can find different parts of a face.
+        </Text>
+        <Text style={styles.secondText}>
+          So do we just take our filters and try them over and over on the
+          image?
+        </Text>
+      </View>
+
+      <View style={styles.footerButtons}>
+        <LessonButton
+          navigation={navigation}
+          nextScreen=""
+          buttonColor={["#32B59D", "#3AC55B"]}
+          buttonText="Yes! 👍"
+        />
+        <LessonButton
+          navigation={navigation}
+          nextScreen=""
+          buttonColor="#8976C2"
+          buttonText="No! 👎"
+        />
+      </View>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  footerButtons: {
+    marginBottom: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textSection: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mainText: {
+    marginVertical: 10,
+    padding: 15,
+    textShadowColor: "rgba(0, 0, 0, 0.1)",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    textAlign: "center",
+    color: "white",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  secondText: {
+    marginVertical: 10,
+    padding: 15,
+    textShadowColor: "rgba(0, 0, 0, 0.1)",
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    textAlign: "center",
+    color: "white",
+    fontSize: 40,
+  },
+});
