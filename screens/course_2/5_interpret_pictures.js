0e80732528a63b652a56dbc26811fa6084d4fd17
afry4321 <afry4321@gmail.com>
2021-10-14 18:41:49 -0500

    Added background image and screen 5
    I coded out screen 5 (5_interpret_pictures) and added the needed background image for that slide into assets (objectregbackground)

--- screens/course_2/5_interpret_pictures.js
+++ screens/course_2/5_interpret_pictures.js
@@ -1,10 +1,52 @@
 // Now that we know how computers interpret photos
 
 import React from 'react'
-import Text from 'react-native'
+import { StyleSheet, View, Text, ImageBackground } from 'react-native'
 
-export default function Course2InterpretPictures () {
+import LessonButton from '../../components/LessonButton'
+
+export default function Course2InterpretPictures({navigation}) {
   return (
-    <Text>5_interpret_pictures.js</Text>
+    <ImageBackground source={require('../../assets/objectregbackground.png')} style={styles.container}>
+      <View style={styles.information}>
+        <Text style={styles.textFont}>Now that we know how computers interpret photos, we will learn a popular face detection strategy.</Text>
+      </View>
+      
+      <View style={styles.footerButtons}>
+          <LessonButton navigation={navigation} nextScreen='Course2Review3' buttonColor='#8976C2' buttonText='Back' />
+          <LessonButton navigation={navigation} nextScreen='Course2OceanNumbers' buttonColor={['#32B59D', '#3AC55B']} buttonText="I'm Ready!" />
+      </View>
+    </ImageBackground>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      paddingHorizontal: 20,
+      paddingVertical: 15,
+  },
+
+  information: {
+    flex: 1,
+    borderRadius: 7,
+    overflow: 'hidden',
+    alignItems: 'center',
+  },
+
+  textFont: {
+    marginTop: 100,
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+
+  footerButtons: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+});
+
