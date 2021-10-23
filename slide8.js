ace161606d4516eebd2309fda78bba55700131c1
AdvayAravind24 <AdvayAravind24@mittymonarch.com>
2021-10-22 20:42:08 -0700

    Update slide8.js

--- slide8.js
+++ slide8.js
@@ -1,32 +1,27 @@
 import React from "react"
-import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity } from 'react-native'
 import { LinearGradient } from "expo-linear-gradient"
 import LessonButton from "../../components/LessonButton"
 
-const screenHeight = Dimensions.get('screen').height;
-const screenWidth = Dimensions.get('screen').width;
-
+const screenHeight = Dimensions.get('screen').height
+const screenWidth = Dimensions.get('screen').width
 
 export default function App ({ navigation }) {
   return (
     <LinearGradient colors={["#8976C2", "#E6E8FB"]} style={styles.container}>
     <View style={styles.lowPicture}>
         <TouchableOpacity>
-          <Image 
+          <Image
             fadeDuration={1000}
-            resizeMode={'stretch'}
-            source={{
-              width: screenWidth,
-              height: screenHeight * 0.35,
-              uri: "https://s3-alpha-sig.figma.com/img/234a/4f5e/ceea7eb66dc606c735ace5b23993d3cd?Expires=1635724800&Signature=GCrAiCVrtCeuA-J5VcFOiqxt6Zg5fqVi0JfFkX1VYy7-swUQTPVIBiBw~1GvphvUMVKbnT15YQDmfriQpiKTZ4JHwfnevnU2YiMUKJQQwfdfWYQCigbV-8d0CnISDLAK07yBRYVMTsUnd~oPjC2cGyuBaGt3hSnRzvHo5p7zcDr4FBy6WAhXA2TFXa34LpodnxxI1D8vWEzYDkTFejT5K~vS5-jVMnYOEpcbmCxiPwjA7okHxbkakv2-RrN2o1ijVn1aqfty5~jH1c57Nxtm3OGLKUcQ0TpX3XeZEKfbK7ETWA8skmv1jSy747VhPeEezPcqb6EEDs6TkVrC4OB2wQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
-            }}
+            resizeMode={'cover'}
+            source={require("../../components/greenWater.png")}
           />
         </TouchableOpacity>
       </View>
     <View style = {styles.lessonContent}> 
       <View style={styles.textSection}>
         <Text style={styles.mainText}>SO</Text>
-        <Text style={styles.secondaryText}>if a photo is an ocean on numbers, how does a computer tell eyes from noses?</Text>
+        <Text style={styles.secondaryText}>If a photo is an ocean on numbers, how does a computer tell eyes from noses?</Text>
       </View>
 
       <View style={styles.footerButtons}> 
@@ -99,9 +94,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold"
   },
   lowPicture: {
-    alignSelf: "flex-end",
+    alignSelf: "flex-start",
     bottom: 0,
     flex: 1,
-    position: "absolute"
+    position: "absolute",
+    width: screenWidth,
+    height: screenHeight * 0.4
   }
 })
