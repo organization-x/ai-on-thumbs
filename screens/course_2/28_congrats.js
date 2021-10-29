c739dab89026ff38781300175f7ca749169c8e8e
LordSunchips <sanjaymanoj04@gmail.com>
2021-10-29 18:09:20 -0500

    Update 28_congrats.js

--- screens/course_2/28_congrats.js
+++ screens/course_2/28_congrats.js
@@ -1,10 +1,93 @@
 // You just learned your first AI algorithm. Do you see how simple it is?
 
 import React from 'react'
-import Text from 'react-native'
+import { StyleSheet, View, Text, Dimensions } from 'react-native'
+import LessonButton from '../../components/LessonButton'
+import { LinearGradient } from 'expo-linear-gradient'
+import ConfettiCannon from 'react-native-confetti-cannon'
 
-export default function Course2Congrats () {
+import * as Analytics from 'expo-firebase-analytics'
+Analytics.setCurrentScreen('Course 1 Screen 19: Congrats Screen')
+
+const width = Dimensions.get('window').width
+const height = Dimensions.get('window').height
+
+export default function Course2Congrats ({ navigation }) {
   return (
-    <Text>28_congrats.js</Text>
+    <LinearGradient colors={['#8976C2', '#E6E8FB']} style={styles.container}>
+      <ConfettiCannon
+        count={100}
+        origin={{ x: width / 2, y: -100 }}
+        fallSpeed={1500}
+        autoStart
+        fadeOut
+      />
+      <View style={styles.interactive}>
+        <Text style={styles.bigText}>Congrats!</Text>
+        <Text style={styles.text}>You just learned your first AI algorithm. </Text>
+        <Text style={styles.text}>Do you see how simple it is?</Text>
+        <Text style={styles.boldText}>Now let’s review.</Text>
+      </View>
+      <View style={styles.footerButtons}>
+        <LessonButton 
+          navigation={navigation} 
+          nextScreen='Course2SearchingAlgorithmMark' 
+          buttonColor='#8976C2' 
+          buttonText='Back' />
+        <LessonButton 
+        navigation={navigation} 
+        nextScreen='Course2AlgorithmReview1' 
+        buttonColor={['#32B59D', '#3AC55B']} 
+        buttonText='Continue' />
+      </View>
+    </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15
+  },
+  interactive: {
+    flex: 1,
+    borderRadius: 7,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+  text: {
+    textShadowColor: 'rgba(0, 0, 0, 0.1)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    marginTop: '15%',
+    textAlign: 'center',
+    color: 'white',
+    fontSize: height / 22
+  },
+  boldText: {
+    textShadowColor: 'rgba(0, 0, 0, 0.1)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    marginTop: height / 20,
+    textAlign: 'center',
+    color: 'white',
+    fontSize: height / 25,
+    fontWeight: 'bold'
+  },
+  bigText: {
+    textShadowColor: 'rgba(0, 0, 0, 0.1)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    marginTop: height / 10,
+    textAlign: 'center',
+    color: 'white',
+    fontSize: height / 13,
+    fontWeight: 'bold'
+  },
+  footerButtons: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
