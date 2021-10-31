be144bb77c07ed14cb675eb7614f1e572e7704cb
RohanJoshi28 <78099546+RohanJoshi28@users.noreply.github.com>
2021-10-31 14:51:15 -0400

    added the draggable filter screens to course 2

---
+++ screens/course_2/18_completed.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+import TopLessonParagraph from '../../components/TopLessonParagraph'
+import LessonButton from '../../components/LessonButton'
+import { LinearGradient } from 'expo-linear-gradient'
+import BottomLessonParagraph from '../../components/BottomLessonParagraph'
+
+export default function Course2DraggableCompleted ({ navigation }) {
+
+  return (
+    <LinearGradient colors={['#8976C2', '#FFFFFF']} style={styles.container}>
+      <View style={{flex: 1}}>
+        <TopLessonParagraph>Move the filter around the face to find a matching pixel pattern.</TopLessonParagraph>
+        <View style={styles.image}>
+            <Image
+            style={{ width: 300, height: 300 }}
+            source={require('../../assets/markcuban_hybrid_with_filter.png')}
+            />
+        </View>
+        <BottomLessonParagraph>This is a great time to celebrate with our computer, since we both can now recognize important facial features!!</BottomLessonParagraph>
+      </View>
+
+      <View style={styles.footerButtons}>
+        <LessonButton navigation={navigation} nextScreen='Course2DraggableFilter' buttonColor='#8976C2' buttonText='Back' />
+        <LessonButton
+          navigation={navigation}
+          nextScreen='Course2DraggableExplanation'
+          buttonColor={['#32B59D', '#3AC55B']}
+          buttonText={'Continue'}
+          actOpacity={1}
+        />
+      </View>
+    </LinearGradient>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15
+  },
+  footerButtons: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  image: {
+      margin: 30,
+  }
+})
--- screens/course_2/18_draggable_filter.js
+++ screens/course_2/18_draggable_filter.js
@@ -1,10 +1,61 @@
-// Move the filter around the face to find a matching pixel pattern.
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import Course2FilterDetection from '../../components/Course2FilterDetection'
+import Tip from '../../components/Tip'
+import LessonHeader from '../../components/TopLessonParagraph'
+import LessonButton from '../../components/LessonButton'
+import ParagraphBox from '../../components/ParagraphBox'
+import { LinearGradient } from 'expo-linear-gradient'
+import TopLessonParagraph from '../../components/TopLessonParagraph'
 
-import React from 'react'
-import Text from 'react-native'
+export default function Course2DraggableFilter ({ navigation }) {
+  const [imageXOffset, setImageXOffset] = useState(0)
+  const [imageYOffset, setImageYOffset] = useState(0)
 
-export default function Course2DraggableFilter () {
+  const [filterText, setFilterText] = useState('')
+
+  const [found, setFound] = useState(false)
+
   return (
-    <Text>18_draggable_filter.js</Text>
+    <LinearGradient colors={['#8976C2', '#FFFFFF']} style={styles.container}>
+      <TopLessonParagraph>Move the filter around the face to find a matching pixel pattern.</TopLessonParagraph>
+      <View
+        style={{ flex: 1 }} onLayout={(event) => {
+          // get dimensions of container and
+          const { x, y } = event.nativeEvent.layout
+          setImageXOffset(x)
+          setImageYOffset(y)
+        }}
+      >
+        <Course2FilterDetection found={found} setFound={setFound} setFilterText={setFilterText} imageXOffset={imageXOffset} imageYOffset={imageYOffset} />
+      </View>
+
+      <ParagraphBox text={`${filterText}`} />
+
+      <View style={styles.footerButtons}>
+        <LessonButton navigation={navigation} nextScreen='Course2RatingContrast' buttonColor='#8976C2' buttonText='Back' />
+        <LessonButton
+          navigation={navigation}
+          nextScreen='Course2DraggableCompleted'
+          buttonColor={found ? ['#32B59D', '#3AC55B'] : '#808080'}
+          buttonText={found ? 'Continue' : 'Not Found'}
+          enabled={found}
+          actOpacity={found ? 0.3 : 1}
+        />
+      </View>
+    </LinearGradient>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15
+  },
+  footerButtons: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+})
