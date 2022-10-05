8e3de02f21a575449a6a947223b91e9750b6949e
reillyn51 <64506642+reillyn51@users.noreply.github.com>
2022-10-05 11:28:54 -0400

    fixed screen 4 and follow up

--- screens/course_4/2.4_hidden_layer_interactive_follow_up.js
+++ screens/course_4/2.4_hidden_layer_interactive_follow_up.js
@@ -1,47 +1,42 @@
-// Distance is how we measure how far apart houses are.
-
 import React from 'react'
-import { StyleSheet, View, Text, Dimensions} from 'react-native'
+import { StyleSheet, View, Text, Dimensions } from 'react-native'
 import LessonButton from '../../components/LessonButton'
 
 import { LinearGradient } from 'expo-linear-gradient'
+import colors from '../../config/colors'
 // import * as Analytics from 'expo-firebase-analytics'
 // Analytics.setCurrentScreen('Course 3 Screen 8: Distance Role')
 
-import colors from '../../config/colors'
-
-import screen_list from '../../config/screen_list'
-import ProgressBar from '../../components/ProgressBar'
+// const [redHouseDisabled, setDisableRedHouse] = React.useState(false)
+// const [blueHouseDisabled, setDisableBlueHouse] = React.useState(false)
+// const [lowerScreenText, setLowerScreenText] = React.useState('Select the house that is closest to the black house.')
 
 const height = Dimensions.get('window').height
 
-export default function Course4page2_5 ({ navigation }) {
+export default function Course4page2_4_correct ({ navigation }) {
   return (
     <View style={styles.container}>
-      {/* <View style={styles.interactive}></View> */}
       <View style={styles.interactive}>
-        
-        {/* <Text style={styles.text}>Now, lets dive further into how NNs process information:</Text>
-        <Text style={styles.boldText}>Backpropagation is one of the most crucial processes of Neural Networks</Text> */}
-        <View style={styles.interactive}>
-        <Text style={styles.boldText}></Text>
-          <Text style={styles.secondText}>Now, lets dive further into how NNs process information:
-          </Text>
-          <Text style={styles.secondText}></Text>
-          <Text style={styles.underlinedText}>Backpropagation </Text> 
-          <Text style={styles.secondText}> is one of the most crucial processes of Neural Networks</Text>
-          
+        <Text style={styles.boldText}>Correct!</Text>
+        <Text style={styles.secondText}>A NN with two or more layers is called a <Text styles={styles.underlinedText}>deep neural network.</Text></Text>
       </View>
-      </View>
+  
       <View style={styles.footerButtons}>
         <LessonButton navigation={navigation} nextScreen='Course4page2_4' buttonColor='#8976C2' buttonText='Back' />
-        <LessonButton navigation={navigation} nextScreen='Course4page2_6' buttonColor={['#32B59D', '#3AC55B']} buttonText="Let's Do It!" />
+        <LessonButton navigation={navigation} nextScreen='Course4page2_5' buttonColor='#8976C2' buttonText='Lets do it!' />    
       </View>
     </View>
   )
 }
 
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    backgroundColor: colors.background
+  },
   secondText: {
     padding: 20,
     marginVertical: 10,
@@ -62,12 +57,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: 'white',
     fontSize: height / 24
-  },
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-    backgroundColor: colors.background
   },
   interactive: {
     flex: 1,
