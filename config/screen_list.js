3a40f42e382804fea910ed15d3452a1a0420f84f
reillyn51 <64506642+reillyn51@users.noreply.github.com>
2022-10-11 11:19:57 -0400

    screen list updated

--- config/screen_list.js
+++ config/screen_list.js
@@ -4,7 +4,7 @@ export default {
     },
     section2: {
         // screens: ['Test9', 'Test1', 'Test2', 'Test3', 'Test7', 'Test8', 'Test1', 'Test2', 'Test3']
-        screens: ['Course4page2_1','Course4page2_2','Course4page2_3','Course4page2_4','Course4page2_5','Course4page2_6','Course4page2_7','Course4page2_8','Course4page2_9']
+        screens: ['Course4page2_1','Course4page2_2','Course4page2_3','Course4page2_4','Course4page2_4_correct','Course4page2_5','Course4page2_6','Course4page2_7','Course4page2_8','Course4page2_9']
     },
     section3: {
         buttonWidth: 30,
